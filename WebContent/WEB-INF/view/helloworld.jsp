6cdd2500e0a9469720a3af50318584cbccacd903
Sharon Tomski <hokiebadger@gmail.com>
2021-08-26 20:12:12 -0500

    initial commit

---
+++ WebContent/WEB-INF/view/helloworld.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World Input Form</title>
+</head>
+<body>
+	<h1>Hello World of Spring!</h1>
+	<br><br>
+	<!-- read name of HTML form field is: studentName -->
+	<h2>Hello ${param.studentName}</h2>
+
+</body>
+
+</html>
---
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.luv2code.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	// controller method to show form
+	
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	// controller method to process the HTML form data
+	
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+}
